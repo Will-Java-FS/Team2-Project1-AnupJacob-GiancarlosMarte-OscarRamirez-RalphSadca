b4851a0ff640f5e535ce85594fa6c5c5030a7702
Oscar Ramirez <oramirezent@gmail.com>
2024-08-09 16:53:45 -0400

    SecurityConfig file added

---
+++ project1/src/main/java/com/revature/Security/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.revature.Security;
+
+public class SecurityConfig {
+
+    // @TODO: Using to map a SecurityFilterChain
+    // takes HttpSecurity and AuthenticationConfiguration
+    // Will use to decide which users are permitted to create which requests
+    // EX: Admins can POST/PUT categories, but Users cannot
+}
