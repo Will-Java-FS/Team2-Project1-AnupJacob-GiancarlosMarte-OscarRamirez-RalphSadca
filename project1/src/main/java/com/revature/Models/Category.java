ace6dd1400f9272d942a432a6e150f75349012da
anupj <coldsteelrean30@gmail.com>
2024-08-09 14:31:12 -0400

    add Category model getter and setter and values

--- project1/src/main/java/com/revature/Models/Category.java
+++ project1/src/main/java/com/revature/Models/Category.java
@@ -1,4 +1,60 @@
 package com.revature.Models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
+
+@Data
+@Entity
+@Table(name = "Category")
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    int category_id;
+    @Column(nullable = false)
+    String name;
+
+    public Category(int category_id, String name) {
+        this.category_id = category_id;
+        this.name = name;
+    }
+
+    public int getCategory_id() {
+        return category_id;
+    }
+
+    public void setCategory_id(int category_id) {
+        this.category_id = category_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return getCategory_id() == category.getCategory_id() && Objects.equals(getName(), category.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCategory_id(), getName());
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "category_id=" + category_id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
