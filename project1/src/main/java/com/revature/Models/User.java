b343d9dd04075319ff620811efbb2d96c26ab1ea
anupj <coldsteelrean30@gmail.com>
2024-08-09 14:14:15 -0400

    add user model

--- project1/src/main/java/com/revature/Models/User.java
+++ project1/src/main/java/com/revature/Models/User.java
@@ -1,4 +1,35 @@
 package com.revature.Models;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name="User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    int user_id;
+    @Column(nullable = false)
+    private String username;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String address;
+    @Column(length = 11)
+    private String phone;
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime created_at = LocalDateTime.now();
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime updated_at = LocalDateTime.now();
+    @Column(length = 10)
+    private String role;
 }
