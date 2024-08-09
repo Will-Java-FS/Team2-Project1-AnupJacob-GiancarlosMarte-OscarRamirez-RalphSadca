f2a9ba36d0edd4e45df0606789af5f677f2b7fa3
Oscar Ramirez <oramirezent@gmail.com>
2024-08-09 14:41:50 -0400

    productModel++

--- project1/src/main/java/com/revature/Models/Product.java
+++ project1/src/main/java/com/revature/Models/Product.java
@@ -5,6 +5,7 @@ import lombok.Data;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Data
 @Entity
@@ -27,4 +28,64 @@ public class Product {
     @Column
     private LocalDateTime time_created;
 
+    public Product() {
+    }
+
+    public Product(int product_id, String title, String description, BigDecimal money, Category category, LocalDateTime time_created) {
+        this.product_id = product_id;
+        this.title = title;
+        this.description = description;
+        this.money = money;
+        this.category = category;
+        this.time_created = time_created;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setMoney(BigDecimal money) {
+        this.money = money;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public void setTime_created(LocalDateTime time_created) {
+        this.time_created = time_created;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return getProduct_id() == product.getProduct_id() && Objects.equals(getTitle(), product.getTitle()) && Objects.equals(getDescription(), product.getDescription()) && Objects.equals(getMoney(), product.getMoney()) && Objects.equals(getCategory(), product.getCategory()) && Objects.equals(getTime_created(), product.getTime_created());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getProduct_id(), getTitle(), getDescription(), getMoney(), getCategory(), getTime_created());
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "product_id=" + product_id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                ", money=" + money +
+                ", category=" + category +
+                ", time_created=" + time_created +
+                '}';
+    }
 }
