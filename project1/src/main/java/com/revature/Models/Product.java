5d919b80e545023939160a1bd58657ddd9cbf995
Oscar Ramirez <oramirezent@gmail.com>
2024-08-09 14:39:57 -0400

    product model

--- project1/src/main/java/com/revature/Models/Product.java
+++ project1/src/main/java/com/revature/Models/Product.java
@@ -1,4 +1,30 @@
 package com.revature.Models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name="Product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int product_id;
+    @Column
+    private String title;
+    @Column
+    private String description;
+    @Column
+    private BigDecimal money;
+    @OneToMany
+    @JoinColumn(name = "category_id")
+    private Category category;
+    @Column
+    private LocalDateTime time_created;
+
 }
