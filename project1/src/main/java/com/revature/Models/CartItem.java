84d515ced5ef67bf00259e8828af6c22aa685d90
ralph-sadca <sadca_r@yahoo.com.ph>
2024-08-09 16:19:17 -0500

    init:CartItem model

---
+++ project1/src/main/java/com/revature/Models/CartItem.java
@@ -0,0 +1,38 @@
+package com.revature.Models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="cart_item")
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class CartItem{
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private int cart_item_id;
+
+    @OneToOne
+    @JoinColumn(name="product_id", nullable = false)
+    private Product product;
+
+    @Column(name="cart_id", nullable = false)
+    private int cart_id;
+
+    @Column(name="quantity")
+    private int quantity;
+
+    @Column(name="created_at",columnDefinition = "TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime created_at;
+
+    @Column(name="modified_at",columnDefinition = "TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime modified_at;
+}
