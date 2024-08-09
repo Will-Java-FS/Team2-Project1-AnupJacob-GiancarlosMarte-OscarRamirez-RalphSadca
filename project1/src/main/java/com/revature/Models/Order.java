9d19fd9e9d5c8d8bdb5aed52ff73818eb0b7d48e
anupj <coldsteelrean30@gmail.com>
2024-08-09 16:13:26 -0400

    update order model

--- project1/src/main/java/com/revature/Models/Order.java
+++ project1/src/main/java/com/revature/Models/Order.java
@@ -1,17 +1,23 @@
 package com.revature.Models;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
 
 public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "cart_id")
-    private int cart_id;
-    @Column
-    private int quantity;
+    @Column(name = "order_id")
+    private int order_id;
     @Column
     private  double total;
+    @Column(nullable = false, columnDefinition = "TIMESTAMP WITHOUT TIME ZONE DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime created_at = LocalDateTime.now();
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+    @OneToOne
+    @JoinColumn(name= "cart_id")
+    private Cart cart;
+
 }
