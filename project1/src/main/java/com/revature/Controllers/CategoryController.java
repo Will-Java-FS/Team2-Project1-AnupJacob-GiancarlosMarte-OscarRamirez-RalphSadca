8223b3cd323fcedebf27100cbacdbb854400668f
ralph-sadca <sadca_r@yahoo.com.ph>
2024-08-11 20:47:03 -0500

    added basic CategoryController/Service

--- project1/src/main/java/com/revature/Controllers/CategoryController.java
+++ project1/src/main/java/com/revature/Controllers/CategoryController.java
@@ -1,4 +1,33 @@
 package com.revature.Controllers;
 
+import com.revature.Models.Category;
+import com.revature.Services.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CategoryController {
+
+    CategoryService cs;
+
+    @Autowired
+    private CategoryController(CategoryService cs){
+        this.cs = cs;
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/category")
+    public ResponseEntity<Category> addCategory(@RequestBody Category category){
+        Category newCategory = cs.addCategory(category);
+        return new ResponseEntity<>(newCategory, HttpStatus.CREATED);
+    }
+
+    @RequestMapping(method=RequestMethod.GET, value="/category")
+    public ResponseEntity<List<Category>> getAllCategories(){
+        List<Category> categories = cs.getAllCategories();
+        return new ResponseEntity<>(categories, HttpStatus.OK);
+    }
 }
--- project1/src/main/java/com/revature/Services/CategoryService.java
+++ project1/src/main/java/com/revature/Services/CategoryService.java
@@ -1,4 +1,27 @@
 package com.revature.Services;
 
+import com.revature.Models.Category;
+import com.revature.Repositories.CategoryRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    CategoryRepo cr;
+
+    @Autowired
+    private CategoryService(CategoryRepo cr){
+        this.cr = cr;
+    }
+
+    public Category addCategory(Category category){
+        return cr.save(category);
+    }
+
+    public List<Category> getAllCategories(){
+        return cr.findAll();
+    }
 }
