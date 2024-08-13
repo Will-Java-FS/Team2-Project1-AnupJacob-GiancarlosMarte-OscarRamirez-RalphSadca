42ac17b4c71f0581839ee98cb7b5f238e89d4db2
ralph-sadca <sadca_r@yahoo.com.ph>
2024-08-12 21:44:03 -0500

    started Order Controller and Service

--- project1/src/main/java/com/revature/Controllers/OrderController.java
+++ project1/src/main/java/com/revature/Controllers/OrderController.java
@@ -1,7 +1,37 @@
 package com.revature.Controllers;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.revature.Models.Order;
+import com.revature.Services.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class OrderController {
+
+    OrderService os;
+
+    @Autowired
+    private OrderController(OrderService os){
+        this.os = os;
+    }
+
+    @RequestMapping(method= RequestMethod.POST, value="/order")
+    public Order postOrder(@RequestBody Order order){
+        return os.postOrder(order);
+    }
+
+    @RequestMapping(method=RequestMethod.GET, value="/order/{user_id}/{order_id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable int user_id, int order_id){
+        Order retrievedOrder = os.getOrderById(user_id, order_id);
+        return retrievedOrder==null ? new ResponseEntity<>(HttpStatus.BAD_REQUEST) : new ResponseEntity<>(retrievedOrder, HttpStatus.OK);
+    }
+
+    @RequestMapping(method=RequestMethod.GET, value="orders/{user_id}")
+    public List<Order> getAllUserOrders(@PathVariable int user_id){
+        return os.getAllUserOrders(user_id);
+    }
 }
