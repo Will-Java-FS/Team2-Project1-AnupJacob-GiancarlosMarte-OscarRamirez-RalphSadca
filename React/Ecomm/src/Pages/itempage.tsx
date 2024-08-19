dcae7fc14b567e84f57ec3af30e1f1df4af57b94
ralph-sadca <sadca_r@yahoo.com.ph>
2024-08-19 16:16:58 -0500

    made itempage list out item comp

--- React/Ecomm/src/Pages/itempage.tsx
+++ React/Ecomm/src/Pages/itempage.tsx
@@ -1,9 +1,34 @@
-import {Item} from "../components/item";
+import Item from "../components/item.tsx";
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
 
 const Itempage=()=>{
+
+    const [item, setItem] = useState('');
+
+    const getData = async () => {
+        const response = await axios.get("http://localhost:8080/products");
+        setItem(response.data);
+    }
+
+    useEffect(() => {
+        getData();
+      }, []);
+
     return(
         <>
-        <Item/>
+        <Container>
+            <Row>
+               {
+               item && item.map && item.map((item, index) => (
+                     <Item index={index} product_id={item.product_id} title={item.title} description={item.description} price={item.price} category={item.category.name}/>
+               ))
+               }
+            </Row>
+         </Container>
         </>
     )
 }
--- React/Ecomm/src/components/item.tsx
+++ React/Ecomm/src/components/item.tsx
@@ -1,10 +1,21 @@
-export function Item(){
-    // const name = "Anup"
-     //short for react.fragment <> </>
-  return(
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.css';
+
+export default function Item(props: {index:number, product_id:number, title:string, description:string, price:number, category:string}) {
+
+   return(
      <> 
-     <h1>All Items</h1>
+      <Card style={{ width: '18rem', margin: '.3rem' }}> 
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+               <Card.Title>{props.title}</Card.Title>
+               <Card.Text>Description: {props.description}</Card.Text>
+               <Card.Text>Price: {props.price}</Card.Text>
+               <Card.Text>Catergory: {props.category}</Card.Text>
+               <Button variant="primary">Add to Cart</Button>
+            </Card.Body>
+      </Card>
      </>
-     
   )
  }
