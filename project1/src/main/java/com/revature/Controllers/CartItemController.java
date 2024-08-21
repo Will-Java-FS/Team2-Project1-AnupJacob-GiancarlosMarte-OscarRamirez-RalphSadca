0d790dda0602ff58e435b0700379c7323da3f4e9
ralph-sadca <sadca_r@yahoo.com.ph>
2024-08-21 10:01:45 -0500

    cart-item repo serv contr

--- project1/src/main/java/com/revature/Controllers/CartItemController.java
+++ project1/src/main/java/com/revature/Controllers/CartItemController.java
@@ -1,5 +1,42 @@
 package com.revature.Controllers;
 
+import com.revature.Models.CartItem;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.revature.Services.CartItemService;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class CartItemController {
 
+    CartItemService cis;
+
+    @Autowired
+    private CartItemController(CartItemService cis) {
+        this.cis = cis;
+    }
+
+//    @RequestMapping(method= RequestMethod.GET, value="/cart-item")
+//    public CartItem getCartItemById(){
+//        return cis.getCartItemById();
+//    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/add-cart-item")
+    public CartItem addCartItem(@RequestBody CartItem cartItem) {
+        return cis.addCartItem(cartItem);
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/get-cart-items/{cart_id}")
+    public List<CartItem> getAllCartItems(@PathVariable int cart_id) {
+        return cis.getCartItems(cart_id);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/delete-cart-item/{cart_item_id}")
+    public void deleteCartItem(@PathVariable int cart_item_id) {
+        cis.deleteCartItem(cart_item_id);
+    }
+
 }
+
--- project1/src/main/java/com/revature/Repositories/CartItemRepo.java
+++ project1/src/main/java/com/revature/Repositories/CartItemRepo.java
@@ -2,11 +2,16 @@ package com.revature.Repositories;
 
 import com.revature.Models.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface CartItemRepo extends JpaRepository<CartItem, Integer> {
+
+    @Query("SELECT c FROM CartItem c WHERE c.cart.cart_id = :cart_id")
+    List<CartItem> findAllByCartId(@Param("cart_id") int cart_id);
 
 }
